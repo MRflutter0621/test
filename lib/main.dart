eb68191525ccc0d873ee5be577792f4be07283fb
MRflutter0621 <mrflutter421@gmail.com>
2024-10-14 19:27:46 +0530

    new test Andriod studio

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.amberAccent),
-      home: HomeScreen(),
+      home:const HomeScreen(),
     );
   }
 }
