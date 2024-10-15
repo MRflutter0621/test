fc3933348df96991fc6b6bab26356fd4e01ab591
MRflutter0621 <mrflutter421@gmail.com>
2024-10-15 16:17:18 +0530

    new vscode

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,11 +7,12 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('New App'),
+        title: const Text('New App'),
         backgroundColor: Colors.redAccent,
-      ),body: Container(
-      color: Colors.amber,
-    ),
+      ),
+      body: Container(
+        color: Colors.amber,
+      ),
     );
   }
 }
