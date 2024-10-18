8bc24435e0cb4dae70361ccfda661265aa4d68dd
MRflutter0621 <mrflutter421@gmail.com>
2024-10-18 17:25:11 +0530

    new assset

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,17 +6,30 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('New App'),
-        backgroundColor: Colors.greenAccent,
-      ),
-      body: Container(
+        appBar: AppBar(
+          title: const Text('New App'),
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
           decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            colors: [Colors.blueGrey, Colors.greenAccent, Colors.blueGrey],
-            begin: Alignment.bottomRight,
-            end: Alignment.topRight),
-      )),
-    );
+            gradient: LinearGradient(
+                colors: [Colors.blueGrey, Colors.greenAccent, Colors.blueGrey],
+                begin: Alignment.bottomRight,
+                end: Alignment.topRight),
+          ),
+          child: const Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Image(
+                  height: 80,
+                  image: AssetImage(
+                      '/Users/akshaybharathan/test/assets/image/Spotify_logo_with_text.svg.png')),
+            ],
+          ),
+        ));
   }
 }
